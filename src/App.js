b655f83168780493ba7c08735c5868e0717d344c
Adam Achebe <mister363636@gmail.com>
2022-11-24 18:50:50 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-content">
+        <div className="left">
+          <div className="header">Receive Finance: Unlock Liquidity in A/R</div>
+          <div className="subheader">
+            Receive allows businesses to tokenize their account receivables and
+            access future funds in a fraction of the time. Our marketplace
+            enables investors to earn guaranteed yield from a previously
+            walled-off asset class.
+          </div>
+        </div>
+
+        <div className="right">
+          <div className="button" id="wallet">
+            Connect wallet
+          </div>
+          <div className="button" id="signup">
+            Sign up <span id="small">(Required for businesses)</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./fonts/Hero-New/Hero New Bold.otf";
+import "./fonts/Hero-New/Hero New Regular.otf";
+import "./fonts/Hero-New/Hero New Medium.otf";
+import "./fonts/Hero-New/Hero New Light.otf";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
